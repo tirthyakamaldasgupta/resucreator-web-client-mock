e972ffd433414425fc8c7c1f4e466b1a1fc41dbb
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2022-12-28 07:23:05 +0530

    Added feature to export a resume to PDF

--- components/resumecontainer.tsx
+++ components/resumecontainer.tsx
@@ -1,6 +1,8 @@
 import {getCookie} from "cookies-next";
 import axios from "axios";
 import {useEffect, useState} from "react";
+import { jsPDF } from "jspdf";
+import {fontStyle} from "html2canvas/dist/types/css/property-descriptors/font-style";
 
 type Props = {
     id: string | string[] | undefined;
@@ -8,6 +10,32 @@ type Props = {
 
 export default function ResumeContainer(props: Props) {
     const [resume, setResume] = useState<Array<{ userId: number; resumeTitle: string; firstName: string; middleName: string; lastName: string; profileSummary: string; id: number }>>([]);
+
+    function exportResumeToPDF() {
+        const document = new jsPDF();
+
+        const resumeTitleTextWidth = document.getStringUnitWidth(resume?.[0]?.resumeTitle) * document.getFontSize() / document.internal.scaleFactor;
+
+        const resumeTitleTextXCoordinate = (document.internal.pageSize.width - resumeTitleTextWidth) / 2;
+
+        document.setFont("normal", "bold")
+        document.text(resume?.[0]?.resumeTitle, resumeTitleTextXCoordinate, 15);
+
+        const fullNameTextWidth = document.getStringUnitWidth(`${resume?.[0]?.firstName} ${resume?.[0]?.middleName} ${resume?.[0]?.lastName}`) * document.getFontSize() / document.internal.scaleFactor;
+
+        const fullNameTextXCoordinate = (document.internal.pageSize.width - fullNameTextWidth) / 2;
+
+        document.setFont("normal", "normal")
+        document.text(`${resume?.[0]?.firstName} ${resume?.[0]?.middleName} ${resume?.[0]?.lastName}`, fullNameTextXCoordinate, 25);
+
+        document.setFont("normal", "bold")
+        document.text("Profile Summary", 12, 40);
+
+        document.setFont("normal", "normal")
+        document.text(resume?.[0]?.profileSummary, 12, 50);
+
+        document.save(`${resume?.[0]?.firstName}-${resume?.[0]?.middleName}-${resume?.[0]?.lastName}-${resume?.[0]?.resumeTitle}.pdf`);
+    }
 
     async function fetchResume(accessToken: string | undefined | null | boolean, userID: string | boolean | null | undefined) {
         const configuration = {
@@ -45,6 +73,7 @@ export default function ResumeContainer(props: Props) {
                     </center>
                     <h2>Profile Summary</h2>
                     <p>{resume?.[0]?.profileSummary}</p>
+                    <button type="button" className="btn btn-primary" onClick={exportResumeToPDF}>Export</button>
                 </div>
             </div>
         </div>
